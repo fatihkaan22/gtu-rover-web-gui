c2ce1d157e69003407f5b2e51b6e828b02625a16
harunalbayrak <rover@gtu.edu.tr>
2021-03-19 23:50:34 +0300

    Added a button to get joint states

--- src/main.js
+++ src/main.js
@@ -14,3 +14,30 @@ ros.on('close',function(){
     console.log('Close');
 });
 
+/*const interval = setInterval(function() {
+
+}, 5000);*/
+
+function getJointStates(){
+    var jointStates = new ROSLIB.Topic({
+        ros : ros,
+        name : '/leo/joint_states',
+        messageType : 'sensor_msgs/JointState'
+    });
+
+    jointStates.subscribe(function (message) {
+        var names = message.name;
+        var positions = message.position;
+        var velocities = message.velocity;
+
+        console.log(message);
+        
+        for(let i=0;i<names.length;++i){
+            console.log(names[i]);
+            console.log(positions[i]);
+            console.log(velocities[i]);
+        }
+
+        jointStates.unsubscribe();
+    });
+}
